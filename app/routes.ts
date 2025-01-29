2caff3083a31dc80a6a3f186ab849a9be140d099
Zandra7 <sandra.eng@live.no>
2025-01-29 14:19:12 +0100

    Oppdaterer base_path og route config

--- app/routes.ts
+++ app/routes.ts
@@ -1,7 +1,8 @@
 import { type RouteConfig, index, route } from "@react-router/dev/routes";
 
 export default [
-  index("routes/home.tsx"),
-  route("isalive", "routes/api.internal.isalive.ts"),
-  route("isready", "routes/api.internal.isready.ts"),
+  index("routes/index.tsx"),
+  route("rapportering-saksbehandling-frontend/isalive", "routes/api.internal.isalive.ts"),
+  route("rapportering-saksbehandling-frontend/isready", "routes/api.internal.isready.ts"),
+  route("rapportering-saksbehandling-frontend/", "routes/home.tsx"),
 ] satisfies RouteConfig;
