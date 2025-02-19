c0758f92106803fc89730921355b109f22359d98
Zandra7 <sandra.eng@live.no>
2025-02-19 13:13:50 +0100

    Fjerner også tall fra prefix route

--- app/routes.ts
+++ app/routes.ts
@@ -2,7 +2,7 @@ import { type RouteConfig, index, prefix, route } from "@react-router/dev/routes
 
 export default [
   route("", "routes/home.tsx"),
-  ...prefix("rapportering/:id", [
+  ...prefix("rapportering/", [
     index("routes/index.tsx"),
     route("isalive", "routes/api.internal.isalive.ts"),
     route("isready", "routes/api.internal.isready.ts"),
