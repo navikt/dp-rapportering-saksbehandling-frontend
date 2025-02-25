329ae75b83c6424e51273bbfe3851cc5a9806206
Thor Håkon Bredesen <thor.hakon.bredesen@gmail.com>
2025-02-25 17:09:32 +0100

    vars

--- vitest/mswTest.test.ts
+++ vitest/mswTest.test.ts
@@ -3,6 +3,5 @@ import { getEnv } from "~/utils/env.utils";
 
 test("fetches user data", async () => {
   const response = await fetch(`${getEnv("DP_RAPPORTERING_URL")}/rapporteringsperioder`);
-
   expect(response.status).toBe(200);
 });
