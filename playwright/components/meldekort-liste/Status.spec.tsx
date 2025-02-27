a61fa32a0a59240a10892a82fcce62ac1c06109a
Zandra7 <sandra.eng@live.no>
2025-02-27 09:51:17 +0100

    Legger til tester for Status-komponenten

---
+++ playwright/components/meldekort-liste/Status.spec.tsx
@@ -0,0 +1,35 @@
+import { test, expect } from "@playwright/experimental-ct-react";
+import { Status } from "~/components/meldekort-liste/Status";
+import { RAPPORTERINGSPERIODE_STATUS } from "~/utils/constants";
+
+test.describe("Status", () => {
+  test("skal vise info tag for status TilUtfylling", async ({ mount }) => {
+    const component = await mount(<Status status={RAPPORTERINGSPERIODE_STATUS.TilUtfylling} />);
+    await expect(component).toHaveClass(/(^|\s)navds-tag--info(\s|$)/g);
+    await expect(component).toContainText(RAPPORTERINGSPERIODE_STATUS.TilUtfylling);
+  });
+
+  test("skal vise success tag for status Innsendt", async ({ mount }) => {
+    const component = await mount(<Status status={RAPPORTERINGSPERIODE_STATUS.Innsendt} />);
+    await expect(component).toHaveClass(/(^|\s)navds-tag--success(\s|$)/g);
+    await expect(component).toContainText(RAPPORTERINGSPERIODE_STATUS.Innsendt);
+  });
+
+  test("skal vise warning tag for status Endret", async ({ mount }) => {
+    const component = await mount(<Status status={RAPPORTERINGSPERIODE_STATUS.Endret} />);
+    await expect(component).toHaveClass(/(^|\s)navds-tag--warning(\s|$)/g);
+    await expect(component).toContainText(RAPPORTERINGSPERIODE_STATUS.Endret);
+  });
+
+  test("skal vise success tag for status Ferdig", async ({ mount }) => {
+    const component = await mount(<Status status={RAPPORTERINGSPERIODE_STATUS.Ferdig} />);
+    await expect(component).toHaveClass(/(^|\s)navds-tag--success(\s|$)/g);
+    await expect(component).toContainText(RAPPORTERINGSPERIODE_STATUS.Ferdig);
+  });
+
+  test("skal vise error tag for status Feilet", async ({ mount }) => {
+    const component = await mount(<Status status={RAPPORTERINGSPERIODE_STATUS.Feilet} />);
+    await expect(component).toHaveClass(/(^|\s)navds-tag--error(\s|$)/g);
+    await expect(component).toContainText(RAPPORTERINGSPERIODE_STATUS.Feilet);
+  });
+});
