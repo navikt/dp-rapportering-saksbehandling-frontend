a3b1e034a50b37388612ffdeb31d22768fda6b2c
Zandra7 <sandra.eng@live.no>
2025-02-27 08:59:57 +0100

    Grupperer og endrer navn på tester

--- playwright/components/meldekort-liste/TypeAktivitet.spec.tsx
+++ playwright/components/meldekort-liste/TypeAktivitet.spec.tsx
@@ -3,11 +3,13 @@ import { TypeAktivitet } from "~/components/meldekort-liste/TypeAktivitet";
 import { aktivitetMapping } from "~/components/meldekort-liste/utils";
 import periode from "../../../mocks/responses/rapporteringsperioder";
 
-test("TypeAktivitet", async ({ mount }) => {
-  const component = await mount(<TypeAktivitet periode={periode[1]} />);
+test.describe("TypeAktivitet", () => {
+  test("skal vise alle aktivitetstypene", async ({ mount }) => {
+    const component = await mount(<TypeAktivitet periode={periode[1]} />);
 
-  await expect(component).toContainText(aktivitetMapping.Arbeid.label);
-  await expect(component).toContainText(aktivitetMapping.Syk.label);
-  await expect(component).toContainText(aktivitetMapping.Fravaer.label);
-  await expect(component).toContainText(aktivitetMapping.Utdanning.label);
+    await expect(component).toContainText(aktivitetMapping.Arbeid.label);
+    await expect(component).toContainText(aktivitetMapping.Syk.label);
+    await expect(component).toContainText(aktivitetMapping.Fravaer.label);
+    await expect(component).toContainText(aktivitetMapping.Utdanning.label);
+  });
 });
