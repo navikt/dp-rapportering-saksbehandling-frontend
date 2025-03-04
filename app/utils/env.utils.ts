eb0e19b0bd1aac763a8fc96d1c219c4c169e75e0
Thor Håkon Bredesen <thor.hakon.bredesen@gmail.com>
2025-03-04 14:14:44 +0100

    Logger miljøvariabler

--- app/utils/env.utils.ts
+++ app/utils/env.utils.ts
@@ -1,10 +1,17 @@
 import type { IEnv } from "./types";
 
+const defaultEnv: IEnv = {
+  IS_LOCALHOST: "false",
+  USE_MSW: "false",
+  DP_RAPPORTERING_URL: "https://dp-rapportering.intern.dev.navn.no",
+  BASE_PATH: "/rapportering",
+};
+
 export function getEnv<T>(value: keyof IEnv): T {
   const env = typeof window !== "undefined" ? window.env : process.env;
 
   // @ts-ignore
-  return (env[value] || env[`VITE_${value}`] || "") as unknown as T;
+  return (env[value] || env[`VITE_${value}`] || defaultEnv[value] || "") as unknown as T;
 }
 
 export const isLocalhost = getEnv("IS_LOCALHOST") === "true";
