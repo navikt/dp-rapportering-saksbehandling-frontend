63717c5bca0c10c8cd1ee0e337bc33639437f049
Thor Håkon Bredesen <thor.hakon.bredesen@gmail.com>
2025-03-04 14:08:08 +0100

    Logger miljøvariabler i test

--- vitest/mswTest.test.ts
+++ vitest/mswTest.test.ts
@@ -2,6 +2,11 @@ import { expect, test } from "vitest";
 import { getEnv } from "~/utils/env.utils";
 
 test("fetches user data", async () => {
+  console.log("BASE_PATH", getEnv("BASE_PATH"));
+  console.log("DP_RAPPORTERING_URL", getEnv("DP_RAPPORTERING_URL"));
+  console.log("IS_LOCALHOST", getEnv("IS_LOCALHOST"));
+  console.log("USE_MSW", getEnv("USE_MSW"));
+
   const response = await fetch(`${getEnv("DP_RAPPORTERING_URL")}/rapporteringsperioder`);
   expect(response.status).toBe(200);
 });
