c126e97170d5e35f3f2872665ab7a6396662d7d0
Zandra7 <sandra.eng@live.no>
2025-03-05 09:01:32 +0100

    Flytter dager ut av uke

---
+++ app/components/meldekort-visning/Dag.tsx
@@ -0,0 +1,44 @@
+import classNames from "classnames";
+import { formatterDag, konverterFraISO8601Varighet } from "~/utils/dato.utils";
+import type { IRapporteringsperiodeDag } from "~/utils/types";
+import styles from "./Uke.module.css";
+
+interface DagProps {
+  dag: IRapporteringsperiodeDag;
+}
+
+const erAktivStil = (dag: IRapporteringsperiodeDag, typer: string[]) =>
+  typer.every((type) => dag.aktiviteter.some((a) => a.type === type));
+
+const dagKnappStyle = (dag: IRapporteringsperiodeDag) => ({
+  [styles.arbeid]: erAktivStil(dag, ["Arbeid"]),
+  [styles.sykdom]: erAktivStil(dag, ["Syk"]),
+  [styles.fravaer]: erAktivStil(dag, ["Fravaer"]),
+  [styles.utdanning]: erAktivStil(dag, ["Utdanning"]),
+  [styles.arbeidOgUtdanning]: erAktivStil(dag, ["Arbeid", "Utdanning"]),
+  [styles.sykOgUtdanning]: erAktivStil(dag, ["Syk", "Utdanning"]),
+  [styles.fravaerOgUtdanning]: erAktivStil(dag, ["Fravaer", "Utdanning"]),
+  [styles.sykOgFravaer]: erAktivStil(dag, ["Syk", "Fravaer"]),
+  [styles.sykFravaerOgUtdanning]: erAktivStil(dag, ["Syk", "Fravaer", "Utdanning"]),
+});
+
+export function Dag({ dag }: DagProps) {
+  return (
+    <td key={dag.dato}>
+      <div className={styles.aktivitetContainer}>
+        <span className={classNames(styles.aktivitet, styles.dato, dagKnappStyle(dag))}>
+          {formatterDag(dag.dato)}
+        </span>
+        {konverterFraISO8601Varighet(
+          dag.aktiviteter?.find((aktivitet) => aktivitet.type === "Arbeid")?.timer ?? ""
+        ) && (
+          <span className={styles.timer}>
+            {konverterFraISO8601Varighet(
+              dag.aktiviteter?.find((aktivitet) => aktivitet.type === "Arbeid")?.timer ?? ""
+            )}
+          </span>
+        )}
+      </div>
+    </td>
+  );
+}
--- app/components/meldekort-visning/Uke.tsx
+++ app/components/meldekort-visning/Uke.tsx
@@ -1,29 +1,9 @@
-import { formatterDag, konverterFraISO8601Varighet } from "~/utils/dato.utils";
 import type { IRapporteringsperiodeDag } from "~/utils/types";
-import { aktivitetMapping } from "~/components/meldekort-liste/utils";
-import styles from "./Uke.module.css";
-import classNames from "classnames";
-
-type AktivitetType = keyof typeof aktivitetMapping;
+import { Dag } from "./Dag";
 
 interface IProps {
   uke: IRapporteringsperiodeDag[];
 }
-
-const erAktivStil = (dag: IRapporteringsperiodeDag, typer: AktivitetType[]) =>
-  typer.every((type) => dag.aktiviteter.some((a) => a.type === type));
-
-const dagKnappStyle = (dag: IRapporteringsperiodeDag) => ({
-  [styles.arbeid]: erAktivStil(dag, ["Arbeid"]),
-  [styles.sykdom]: erAktivStil(dag, ["Syk"]),
-  [styles.fravaer]: erAktivStil(dag, ["Fravaer"]),
-  [styles.utdanning]: erAktivStil(dag, ["Utdanning"]),
-  [styles.arbeidOgUtdanning]: erAktivStil(dag, ["Arbeid", "Utdanning"]),
-  [styles.sykOgUtdanning]: erAktivStil(dag, ["Syk", "Utdanning"]),
-  [styles.fravaerOgUtdanning]: erAktivStil(dag, ["Fravaer", "Utdanning"]),
-  [styles.sykOgFravaer]: erAktivStil(dag, ["Syk", "Fravaer"]),
-  [styles.sykFravaerOgUtdanning]: erAktivStil(dag, ["Syk", "Fravaer", "Utdanning"]),
-});
 
 export function Uke({ uke }: IProps) {
   if (!uke?.length) return null;
@@ -31,24 +11,7 @@ export function Uke({ uke }: IProps) {
   return (
     <tr>
       {uke.map((dag) => (
-        <td key={dag.dato}>
-          {" "}
-          {/* TODO: være egen komponent */}
-          <div className={styles.aktivitetContainer}>
-            <span className={classNames(styles.aktivitet, styles.dato, dagKnappStyle(dag))}>
-              {formatterDag(dag.dato)}
-            </span>
-            {konverterFraISO8601Varighet(
-              dag.aktiviteter?.find((aktivitet) => aktivitet.type === "Arbeid")?.timer ?? ""
-            ) && (
-              <span className={styles.timer}>
-                {konverterFraISO8601Varighet(
-                  dag.aktiviteter?.find((aktivitet) => aktivitet.type === "Arbeid")?.timer ?? ""
-                )}
-              </span>
-            )}
-          </div>
-        </td>
+        <Dag key={dag.dato} dag={dag} />
       ))}
     </tr>
   );
