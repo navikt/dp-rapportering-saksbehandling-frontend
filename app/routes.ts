8667957e5e5ea4714fa5cf922b5e4a980f2c0d44
Thor Håkon Bredesen <thor.hakon.bredesen@gmail.com>
2025-03-05 14:16:25 +0100

    Fungerer dette?

--- app/routes.ts
+++ app/routes.ts
@@ -1,9 +1,9 @@
 import { type RouteConfig, index, prefix, route } from "@react-router/dev/routes";
 
 export default [
-  index("routes/index.tsx"),
-  route("/behandling", "routes/behandling.tsx"),
-  route("/behandling/:id", "routes/behandling.$id.tsx"),
-  route("/api/internal/isalive", "routes/api.internal.isalive.ts"),
-  route("/api/internal/isready", "routes/api.internal.isready.ts"),
+  route("/rapportering", "routes/index.tsx"),
+  route("/rapportering/behandling", "routes/behandling.tsx"),
+  route("/rapportering/behandling/:id", "routes/behandling.$id.tsx"),
+  route("/rapportering/api/internal/isalive", "routes/api.internal.isalive.ts"),
+  route("/rapportering/api/internal/isready", "routes/api.internal.isready.ts"),
 ] satisfies RouteConfig;
