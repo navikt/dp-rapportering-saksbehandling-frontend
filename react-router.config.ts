e423c5998a6b563dd8b60e83c73b2aa9b85cf2e1
Thor Håkon Bredesen <thor.hakon.bredesen@gmail.com>
2025-03-05 13:57:59 +0100

    Prøver noe configs

--- react-router.config.ts
+++ react-router.config.ts
@@ -2,6 +2,4 @@ import type { Config } from "@react-router/dev/config";
 
 export default {
   basename: "/rapportering",
-  // Config options...
-  // Server-side render by default, to enable SPA mode set this to `false`
 } satisfies Config;
