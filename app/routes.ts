da1b8f05c06704c81f06b698b4a99c101cbac911
Thor Håkon Bredesen <thor.hakon.bredesen@gmail.com>
2025-03-05 15:04:53 +0100

    bare dev routes

--- app/routes.ts
+++ app/routes.ts
@@ -1,18 +1,18 @@
 import { type RouteConfig, route } from "@react-router/dev/routes";
 
 const routes = [
-  { file: "", path: "routes/index.tsx" },
-  { file: "behandling", path: "routes/behandling.tsx" },
-  { file: "behandling/:id", path: "routes/behandling.$id.tsx" },
-  { file: "api/internal/isalive", path: "routes/api.internal.isalive.ts" },
-  { file: "api/internal/isready", path: "routes/api.internal.isready.ts" },
+  { file: "/rapportering/", path: "routes/index.tsx" },
+  { file: "/rapportering/behandling", path: "routes/behandling.tsx" },
+  { file: "/rapportering/behandling/:id", path: "routes/behandling.$id.tsx" },
+  { file: "/rapportering/api/internal/isalive", path: "routes/api.internal.isalive.ts" },
+  { file: "/rapportering/api/internal/isready", path: "routes/api.internal.isready.ts" },
 ];
 
-if (process.env.NODE_ENV !== "development") {
-  routes.map((route) => ({
-    ...route,
-    file: `/rapportering/${route.file}`,
-  }));
-}
+// if (process.env.NODE_ENV !== "development") {
+//   routes.map((route) => ({
+//     ...route,
+//     file: `/rapportering/${route.file}`,
+//   }));
+// }
 
 export default routes.map((r) => route(r.file, r.path)) satisfies RouteConfig;
