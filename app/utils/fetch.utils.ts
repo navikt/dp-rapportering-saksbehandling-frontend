5f78e8e317446a56e531313c5a11b9a0c83289bf
Thor Håkon Bredesen <thor.hakon.bredesen@gmail.com>
2025-03-12 16:42:10 +0100

    generateCorralationId -> generateCorrelationId, thanks Copilot

--- app/utils/fetch.utils.ts
+++ app/utils/fetch.utils.ts
@@ -6,7 +6,7 @@ import { getSessionId } from "../mocks/session";
 import { getMicrosoftOboToken } from "./auth.utils.server";
 import { isLocalhost } from "./env.utils";
 
-function generateCorralationId() {
+function generateCorrelationId() {
   // https://github.com/navikt/dp-rapportering-frontend/pull/242#pullrequestreview-2403834306
   // korralasjon_id i dp-rappoortering kan være på maks 54 tegn
   return `dp-rapp-${uuidv7()}`.substring(0, 54);
@@ -19,7 +19,7 @@ export async function getHeaders(request: Request, customHeaders = {}) {
     "Content-Type": "application/json",
     Accept: "application/json",
     Authorization: `Bearer ${onBehalfOfToken}`,
-    "X-Request-ID": generateCorralationId(),
+    "X-Request-ID": generateCorrelationId(),
     connection: "keep-alive",
     Referer: request.url,
     ...customHeaders,
