57274b9d19adf99825633b3c089548911dd7988e
Zandra7 <sandra.eng@live.no>
2025-03-12 14:43:34 +0100

    Endrer fra bruker til person

--- app/routes/person.$id.tsx
+++ app/routes/person.$id.tsx
@@ -7,7 +7,7 @@ import { hentRapporteringsperioder } from "~/models/rapporteringsperiode.server"
 import styles from "~/route-styles/rapportering.module.css";
 import type { IRapporteringsperiode } from "~/utils/types";
 
-import type { Route } from "./+types/bruker.$id";
+import type { Route } from "./+types/person.$id";
 
 export async function loader({
   request,
--- app/routes/person.tsx
+++ app/routes/person.tsx
@@ -3,7 +3,7 @@ import { Link } from "react-router";
 export default function Behandling() {
   return (
     <p>
-      Du må velge en bruker, f.eks. <Link to="/bruker/17051412345">17051412345</Link>
+      Du må velge en person, f.eks. <Link to="/person/17051412345">17051412345</Link>
     </p>
   );
 }
