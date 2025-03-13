6fd575ade2b320d059a92ade39d876b2aa9ac862
Thor Håkon Bredesen <thor.hakon.bredesen@gmail.com>
2025-03-13 08:57:02 +0100

    ADd favicon and env vars to playwright

--- app/root.tsx
+++ app/root.tsx
@@ -66,6 +66,23 @@ export const links: Route.LinksFunction = () => [
     rel: "stylesheet",
     href: "https://fonts.googleapis.com/css2?family=Inter:ital,opsz,wght@0,14..32,100..900;1,14..32,100..900&display=swap",
   },
+  {
+    rel: "icon",
+    type: "image/png",
+    sizes: "32x32",
+    href: `/favicon-32x32.png`,
+  },
+  {
+    rel: "icon",
+    type: "image/png",
+    sizes: "16x16",
+    href: `/favicon-16x16.png`,
+  },
+  {
+    rel: "icon",
+    type: "image/x-icon",
+    href: `/favicon.ico`,
+  },
 ];
 
 export function Layout({ children }: { children: React.ReactNode }) {
