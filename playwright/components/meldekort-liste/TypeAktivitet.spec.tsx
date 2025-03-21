a5e1f6bb8813b02de27a794d23241e4b0e366d96
Thor Håkon Bredesen <thor.hakon.bredesen@gmail.com>
2025-03-21 14:25:25 +0100

    Riktigere mock-data, men Playwright-testene feiler

--- playwright/components/meldekort-liste/TypeAktivitet.spec.tsx
+++ playwright/components/meldekort-liste/TypeAktivitet.spec.tsx
@@ -2,11 +2,42 @@ import { expect, test } from "@playwright/experimental-ct-react";
 
 import { TypeAktivitet } from "~/components/rapporteringsperiode-liste/TypeAktivitet";
 import { aktivitetMapping } from "~/components/rapporteringsperiode-liste/utils";
-import periode from "~/mocks/data/mock-rapporteringsperioder";
+import { lagRapporteringsperiode } from "~/mocks/mock-rapporteringsperioder.utils";
+import { AKTIVITET_TYPE } from "~/utils/constants";
 
 test.describe("TypeAktivitet", () => {
   test("skal vise alle aktivitetstypene", async ({ mount }) => {
-    const component = await mount(<TypeAktivitet periode={periode[1]} />);
+    const component = await mount(
+      <TypeAktivitet
+        periode={lagRapporteringsperiode({
+          dager: [
+            {
+              dagIndex: 0,
+              dato: "2025-01-01",
+              aktiviteter: [
+                {
+                  type: AKTIVITET_TYPE.Arbeid,
+                  dato: "2025-01-01",
+                  timer: "PT2H",
+                },
+                {
+                  type: AKTIVITET_TYPE.Fravaer,
+                  dato: "2025-01-01",
+                },
+                {
+                  type: AKTIVITET_TYPE.Syk,
+                  dato: "2025-01-01",
+                },
+                {
+                  type: AKTIVITET_TYPE.Utdanning,
+                  dato: "2025-01-01",
+                },
+              ],
+            },
+          ],
+        })}
+      />
+    );
 
     await expect(component).toContainText(aktivitetMapping.Arbeid.label);
     await expect(component).toContainText(aktivitetMapping.Syk.label);
