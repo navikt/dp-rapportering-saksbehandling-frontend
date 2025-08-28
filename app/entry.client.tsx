93bf82656926da2390cc1e934de96357957ac453
abdifatah-bashi <abdifatah.bashi@nav.no>
2025-08-28 14:20:40 +0200

    Fikser byggfeil

--- app/entry.client.tsx
+++ app/entry.client.tsx
@@ -3,13 +3,8 @@ import { hydrateRoot } from "react-dom/client";
 import { HydratedRouter } from "react-router/dom";
 
 import { initFaro } from "./faro";
-import { logger } from "./models/logger.server";
 
-try {
-  initFaro();
-} catch (error) {
-  logger.error(error);
-}
+initFaro();
 
 startTransition(() => {
   hydrateRoot(
