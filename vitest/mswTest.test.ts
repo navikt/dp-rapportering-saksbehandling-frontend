5acb9f22328d0bf934de8db5f1a85935c24e6ccb
Thor Håkon Bredesen <thor.hakon.bredesen@gmail.com>
2025-09-03 09:53:40 +0200

    Oppdaterer test med personId som finnes i datasettet

--- vitest/mswTest.test.ts
+++ vitest/mswTest.test.ts
@@ -1,10 +1,11 @@
 import { expect, test } from "vitest";
 
+import { mockPersons } from "~/mocks/data/mock-persons";
 import { getEnv } from "~/utils/env.utils";
 
 test("fetches user data", async () => {
   const response = await fetch(
-    `${getEnv("DP_MELDEKORTREGISTER_URL")}/sb/person/132456789/meldekort`,
+    `${getEnv("DP_MELDEKORTREGISTER_URL")}/sb/person/${mockPersons[0].id}/meldekort`,
   );
   expect(response.status).toBe(200);
 });
